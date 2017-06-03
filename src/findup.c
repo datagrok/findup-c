6159a63dbde1db9965a7b701e9cb78e2c9e3e87b
Michael F. Lamb <mike@datagrok.org>
2017-06-02 21:00:59 -0700

    minor changes while trying to figure out gnulib

--- src/findup.c
+++ src/findup.c
@@ -18,8 +18,10 @@
  * */
 
 #define version "0.1"
+#define PROGRAM_NAME "findup"
+#define AUTHORS proper_name ("Michael F. Lamb")
 
-#include <fcntl.h>
+
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,21 +30,44 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void usage() {
+#include "canonicalize.h"
+
+#define FINDUP_MODE_MASK (FINDUP_ONEFS | FINDUP_NOLINKS)
+enum findup_flags_t {
+    FINDUP_ONEFS   = 1<<0,
+    FINDUP_NOLINKS = 1<<1,
+};
+typedef enum findup_flags_t findup_flags_t;
+
+void usage(void);
+void license(void);
+int findup(char*, char*, int, int, int);
+
+void usage(void) {
     fputs("Usage: findup [OPTION]... FILENAME\n", stdout);
-    fputs("find the nearest ancestor directory containing FILENAME\n", stdout);
+    fputs("find FILENAME in the nearest ancestor directory\n", stdout);
     fputs("\
     -h              display this help and exit\n\
     -V              display version and license information\n\
     -C DIRECTORY    begin search in DIRECTORY (default: current directory)\n\
     -q              suppress all normal output\n\
     -a              print paths to FILENAME found in all ancestor directories\n\
-    -x              skip directories on different file systems than default\n\
+    -x              don't traverse other file systems\n\
+    -P              resolve path symlinks before walking hierarchy\n\
+\n\
+The effect of -P is as follows. Consider a filesystem such that the path\n\
+/a/b/c/ exists and /a/c is a symlink to /a/b/c/. Then,\n\
+\n\
+'findup -C /a/c foo' will examine:  'findup -PC /a/c foo' will examine:\n\
+/a/c/foo                            /a/b/c/foo\n\
+/a/foo                              /a/b/foo\n\
+/foo                                /a/foo\n\
+                                    /foo\n\
 \n\
 Exit status is 0 if FILENAME is found, 1 if not, 2 upon error.\n", stdout);
 }
 
-void license() {
+void license(void) {
     printf("findup %s\n", version);
     fputs("Copyright (C) 2017  Michael F. Lamb\n\
 License: AGPLv3+ (GNU AGPL version 3 or later) <http://gnu.org/licenses/agpl.html>.\n\
@@ -62,7 +87,8 @@ See the source code or license text for details.\n\\n\
 
 
 int findup(char *dir, char *filename, int quiet, int all, int onefs) {
-    int dirlen, i;
+    size_t dirlen;
+    int i;
     struct stat sb, root_sb;
     if (stat("/", &root_sb) != 0) {
        fputs("can't stat root!\n", stdout);
@@ -76,6 +102,8 @@ int findup(char *dir, char *filename, int quiet, int all, int onefs) {
         if (stat(dir, &sb) == 0) {
             printf("found: %s\n", dir);
             return 0;
+        } else {
+            perror("Error:");
         }
         strncpy(&dir[dirlen], "../", 4);
         dirlen += 3;
@@ -88,8 +116,9 @@ int findup(char *dir, char *filename, int quiet, int all, int onefs) {
 int main(int argc, char **argv) {
     int optc;
     int quiet, all, onefs = 0;
-    int dirfd = AT_FDCWD;
-    char dir[PATH_MAX] = "";
+    char dir[PATH_MAX];
+
+    getcwd(dir, PATH_MAX);
 
     while ((optc = getopt(argc, argv, "hVC:qax")) != -1) {
         switch (optc) {
